7d63682a45913dbd87f059929f028d94cd5b1e74
Alex Fuhr <afuhrtrumpet@gmail.com>
2014-03-22 13:54:58 +0000

    Changed packages to work on raspberry pi

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var port = 3700;
-var Lcd = require('lcd'), lcd = new Lcd({rs:25, e:24, data:[23, 17, 21, 22], cols:8, rows:2});
+var Lcd = require('lcd'), lcd = new Lcd({rs:25, e:24, data:[23, 17, 27, 22], cols:8, rows:2});
 
 lcd.on('ready', function () {
   setInterval(function () {
