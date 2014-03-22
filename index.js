6198bdce1519d4c748387cfec755aee666c84017
Alex Fuhr <afuhrtrumpet>
2014-03-22 09:32:38 -0400

    Added beginning LCD code

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = 3700;
+var Lcd = require('lcd'), lcd = new Lcd({rs:25, e:24, data:[23, 17, 21, 22], cols:8, rows:2});
+
+lcd.on('ready', function () {
+  setInterval(function () {
+    lcd.setCursor(0, 0);
+    lcd.print(new Date().toISOString().substring(11, 19));
+  }, 1000);
+});
