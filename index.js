611fa0f091dc286aa78614a4d1ca1179f6776c99
Alex Fuhr <afuhrtrumpet@gmail.com>
2014-03-23 12:22:06 +0000

    Working, but now getting Eg EBUSY error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var port = 3700;
-var Lcd = require('lcd'), lcd = new Lcd({rs:25, e:24, data:[23, 17, 27, 22], cols:16, rows:2});
+var Lcd = require('lcd'), lcd = new Lcd({rs:25, e:24, data:[23, 16, 27, 22], cols:16, rows:2});
 
 app.set('views', __dirname + '/tpl')
 app.set('view engine', "jade");
@@ -18,17 +18,19 @@ var io = require('socket.io').listen(app.listen(port));
 io.sockets.on('connection', function(socket) {
 	socket.on('send', function(data) {
 		lcd.clear();
-		lcd.setCursor(0, 0);
-		if (data.message.length > 16) {
-			lcd.print(data.message.substring(0, 17));
-			var printSecondLine = function() {
-				lcd.setCursor(0, 1);
-				lcd.print(data.message.substring(17));
-			};
-			setTimeout(printSecondLine, 500);
-		} else {
-			lcd.print(data.message);
-		}
+		lcd.once('clear', function() {
+			lcd.setCursor(0, 0);
+			if (data.message.length > 16) {
+				lcd.print(data.message.substring(0, 16));
+				var printSecondLine = function() {
+					lcd.setCursor(0, 1);
+					lcd.print(data.message.substring(16));
+				};
+				setTimeout(printSecondLine, 300);
+			} else {
+				lcd.print(data.message);
+			}
+		});
 	});
 });
 
