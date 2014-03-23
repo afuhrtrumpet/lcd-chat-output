5217f466be0d30c060719b4f7144be34bff3ef1b
Alex Fuhr <afuhrtrumpet>
2014-03-23 18:42:04 -0400

    Added untested scrolling functionality with messages.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var port = 3700;
 var Lcd = require('lcd'), lcd = new Lcd({rs:25, e:24, data:[23, 17, 27, 22], cols:16, rows:2});
+var Gpio = require('onoff').Gpio;
 
 app.set('views', __dirname + '/tpl')
 app.set('view engine', "jade");
@@ -15,23 +16,53 @@ app.use(express.static(__dirname + '/public'));
 
 var io = require('socket.io').listen(app.listen(port));
 
+var upButton = { PIN : 18, GPIO : null };
+var downButton = { PIN : 4, GPIO : null};
+
+var messages = [""];
+var index = 0;
+
+var printMessage = function() {
+	message = messages[index]
+	lcd.clear();
+	lcd.once('clear', function() {
+		lcd.setCursor(0, 0);
+		if (message.length > 16) {
+			lcd.print(message.substring(0, 16));
+			var printSecondLine = function() {
+				lcd.setCursor(0, 1);
+				lcd.print(message.substring(16));
+			};
+			setTimeout(printSecondLine, 300);
+		} else {
+			lcd.print(message);
+		}
+	});
+}
+
 io.sockets.on('connection', function(socket) {
 	socket.on('send', function(data) {
-		lcd.clear();
-		lcd.once('clear', function() {
-			lcd.setCursor(0, 0);
-			if (data.message.length > 16) {
-				lcd.print(data.message.substring(0, 16));
-				var printSecondLine = function() {
-					lcd.setCursor(0, 1);
-					lcd.print(data.message.substring(16));
-				};
-				setTimeout(printSecondLine, 300);
-			} else {
-				lcd.print(data.message);
-			}
-		});
+		messages.push(data.message);
+		index = messages.length - 1;
+		printMessage();
 	});
+
+	upButton.GPIO = new Gpio(upButton.PIN, 'in', 'both');
+	downButton.GPIO = new Gpio(downButton.PIN, 'in', 'both');
+	upButton.GPIO.watch(function(err, val) {
+		if (err) throw err;
+		if (index < 0) {
+			index--;
+			printMessage();
+		}
+	}
+	downButton.GPIO.watch(function(err, val) {
+		if (err) throw err;
+		if (index < messages.length + 1) {
+			index++;
+			printMessage();
+		}
+	}
 });
 
 console.log("Listening on port " + port);
