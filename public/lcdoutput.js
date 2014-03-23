fb36f23f3b1212b4c7f3540e1bf32f4740a617fa
Alex Fuhr <afuhrtrumpet@gmail.com>
2014-03-23 03:02:13 +0000

    Works, but text becomes garbled after a while

--- public/lcdoutput.js
+++ public/lcdoutput.js
@@ -5,7 +5,7 @@ window.onload = function() {
 	var sendButton = document.getElementById("send");
 
 	sendButton.onclick = function() {
-		var message = text.value;
-		socket.emit('send', { message: text });
+		var input = text.value;
+		socket.emit('send', { message: input });
 	};
 }
